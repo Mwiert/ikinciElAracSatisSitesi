f705e71464954561d80052d4fe13db4e93a2e05f
Kayra Akbayramlar <kayyraakbayramlar@hotmail.com>
2022-05-25 00:08:19 +0300

    Login ve register view + controller eklendi

---
+++ ikinciElAracSatisSitesi/Controllers/RegisterController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ikinciElAracSatisSitesi.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetRegister()
+        {
+            return View();
+        }
+    }
+}
